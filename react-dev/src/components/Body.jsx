0153045f580d88adbc10cd1c878c80c6615e805b
amitk <amitk@amitx13>
2023-11-01 23:02:49 +0530

    added searchbar functionality

--- react-dev/src/components/Body.jsx
+++ react-dev/src/components/Body.jsx
@@ -1,4 +1,29 @@
+import { useState } from "react";
 import restaurantList from "../assets/RestaurantData/Restaurant"
+
+/* function filtersearch(searchText,searchRestaurant){
+  const val = searchRestaurant.filter((Restaurant)=>{
+    Restaurant.info.name.includes(searchText);
+  }
+  )
+  return val;
+} */
+
+
+function filtersearch(inputValue, searchRestaurant) {
+
+  const filteredResults = searchRestaurant.filter((Restaurant) => {
+    console.log("Restaurant.info.name:", Restaurant.info.name);
+    if(Restaurant.info.name.toLowerCase().includes(inputValue.toLowerCase())){
+      return true;
+    }
+
+  });
+  
+  return filteredResults;
+}
+
+
 
 
 const RestaurantCard = (props)=>{
@@ -16,12 +41,26 @@ const RestaurantCard = (props)=>{
 }
 
 const Body = () => {
+  const [searchText,setSearchText]= useState('')
+  const [searchRestaurant , setSearchRestaurant] = useState(restaurantList);
+
+
   return (
-    <div className='restaurantcard'>
-      {restaurantList.map((restaurant) => (
+    <>
+      <input type="text" className="searchBar" onChange={(e)=>{
+        setSearchText(e.target.value);
+      }} />
+      <button type="submit" className="searchButton" onClick={()=>{
+        const data = filtersearch(searchText,searchRestaurant)
+        console.log(data);
+        setSearchRestaurant(data);
+      }}>Search</button>
+      <div className='restaurantcard'>
+      {searchRestaurant.map((restaurant) => (
         <RestaurantCard restaurant={restaurant} key={restaurant.info.id} />
       ))}
     </div>
-  );
+    </>
+  )
 }
 export default Body;
--- react-dev/src/components/Header.jsx
+++ react-dev/src/components/Header.jsx
@@ -1,37 +1,14 @@
-import { useState } from "react"
-import restaurant from "../assets/RestaurantData/Restaurant"
-
 const Title = ()=>{
   return <h1>Swiggy</h1>
 }
-const Header = () =>{
-  const [inputValue,setinputValue] = useState("");
-  const [searchRestaurant, setSearchRestaurant] = useState(restaurant);
 
-  function filtersearch(inputValue){
-    restaurant.filter((input)=>{
-      if((inputValue.toLowerCase() === (input?.info?.name).toLowerCase())){
-         return 
-      }
-    })
-  }
+const Header = () =>{
 
   return (
   <div className='Header'>
     <Title/>
     <div className="nav-items">
       <ul>
-        <input type="search" className="searchInput"placeholder="search" value={inputValue}
-        onChange={(e)=>{
-          setinputValue(e.target.value);
-        }}/>
-        <button type="submit" 
-        onClick={(e)=>{
-          const data = filtersearch(inputValue);
-          setSearchRestaurant(data);
-        }}>
-          search
-          </button>
         <span>offers</span>
         <span>Help</span>
         <span>Profile</span>
@@ -41,5 +18,4 @@ const Header = () =>{
     </div>
   )
 }
-
 export default Header;
